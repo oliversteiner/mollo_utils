5a79851d12e5379cc4664d4473aebb6a590afcea
Oliver Steiner <oliver@mollo.ch>
2020-07-14 18:05:01 +0200

    New from Template

--- assets/js/mollo_mollo_utils.js
+++ assets/js/mollo_mollo_utils.js
@@ -3,8 +3,8 @@
     attach(context, settings) {
       console.log("Mollo Artist");
 
-        $('#mollo-modultemplate', context)
-          .once('mollo-modultemplate')
+        $('#mollo-utils', context)
+          .once('mollo-utils')
           .each(() => {});
 
     },
