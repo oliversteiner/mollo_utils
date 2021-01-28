92a96bcca3d36732a248ed3ebf2fe198b8f9afc1
Oliver Steiner <oliver@mollo.ch>
2021-01-28 02:39:14 +0100

    add twig var for Owner Full Name

--- src/Utility/MolloUtilsUser.php
+++ src/Utility/MolloUtilsUser.php
@@ -5,13 +5,16 @@ namespace Drupal\mollo_utils\Utility;
 
 
 use Drupal\user\Entity\User;
+use Exception;
 
-class MolloUtilsUser {
+class MolloUtilsUser
+{
 
   /**
    * @return mixed
    */
-  public static function greetingMessageToUser(): string {
+  public static function greetingMessageToUser(): string
+  {
     /* Greeting Message */
     $user_name = \Drupal::currentUser()->getDisplayName();
 
@@ -23,12 +26,10 @@ class MolloUtilsUser {
       $numeric_date = date("G");
       if ($numeric_date >= 0 && $numeric_date <= 11) {
         $greeting = t("Good Morning");
-      }
-      else {
+      } else {
         if ($numeric_date >= 12 && $numeric_date <= 17) {
           $greeting = t("Good Day");
-        }
-        else {
+        } else {
           if ($numeric_date >= 18 && $numeric_date <= 23) {
             $greeting = t("Good Evening");
           }
@@ -39,4 +40,43 @@ class MolloUtilsUser {
     return '';
   }
 
+  public static function userFullName($user_id = false): string
+  {
+    if ($user_id) {
+      $user = User::load($user_id);
+    } else {
+      $user = User::load(\Drupal::currentUser()->id());
+    }
+
+    if ($user && !empty($user)) {
+      $first_name = self::getFieldValue($user, 'field_first_name');
+      $last_name = self::getFieldValue($user, 'field_last_name');
+      // return $user_id;
+         return $first_name . ' ' . $last_name;
+    }
+    return '';
+
+  }
+
+  public static function getFieldValue($user, $field_name)
+  {
+    try {
+      if (
+        $field_name &&
+        $user->hasField($field_name) &&
+        $user->get($field_name)
+      ) {
+        $value = $user->get($field_name)->getValue();
+        if ($value && $value[0] && isset($value[0]['value'])) {
+          return $value[0]['value'];
+        }
+      }
+    } catch (Exception $e) {
+      return '';
+    }
+
+  }
+
 }
+
+
