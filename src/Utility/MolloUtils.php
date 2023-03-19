22b6f8645b46b1afee48d4666153a4ca8d834f51
Oliver Steiner <oliver@mollo.ch>
2023-03-19 19:00:28 +0100

    add function getOrigin

--- src/Utility/MolloUtils.php
+++ src/Utility/MolloUtils.php
@@ -718,5 +718,40 @@ class MolloUtils
 
     return $templates;
   }
+  public static function getOrigin($name)
+  {
+    $vid = 'smmg_origin';
+    $tid = 0;
+
+    $term_list = [];
+    $term_names = [];
+
+    // Load Origin Terms
+    $terms = \Drupal::entityTypeManager()
+      ->getStorage('taxonomy_term')
+      ->loadTree($vid);
+    foreach ($terms as $term) {
+      $term_list[$term->name] = $term->tid;
+      $term_names[] = $term->name;
+    }
+
+    // add Term $name if not in list
+    if (in_array($name, $term_names, true)) {
+      $tid = $term_list[$name];
+    } else {
+      try {
+        $new_tid = Term::create([
+          'name' => $name,
+          'vid' => $vid,
+        ])->save();
+
+        $tid = $new_tid;
+      } catch (EntityStorageException $e) {
+      }
+    }
+
+    // return Term ID
+    return $tid;
+  }
 
 }
